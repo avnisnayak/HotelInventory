4e8a43c05fbf96576ba7b6b035865b13b5151d1c
Avni <avnisnayak96@gmail.com>
2023-11-23 17:23:53 +0000

    Code update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,71 @@ export class LoginComponent {
 
   constructor(private router: Router, private loginService: LoginService) {}
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        // use either syntax
+        // roomId: new FormControl(
+        //   { disabled: true },
+        //   { validators: [Validators.required] }
+        // ),
+        roomId: new FormControl(
+          { value: this.selectRoom.roomNumber, disabled: true },
+          {
+            // updateOn: 'blur',
+            validators: [Validators.required],
+          }
+        ),
+        guestEmail: new FormControl('', {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.email],
+        }),
+        checkinDate: new FormControl('', {
+          validators: [Validators.required],
+        }),
+        checkoutDate: new FormControl('', {
+          validators: [Validators.required],
+        }),
+        bookingStatus: new FormControl('', {
+          validators: [Validators.required],
+        }),
+        bookingAmount: new FormControl('', {
+          validators: [Validators.required],
+        }),
+        bookingDate: new FormControl(''),
+        mobileNumber: new FormControl('', { updateOn: 'blur' }),
+        guestName: new FormControl('', {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.minLength(5)],
+        }),
+        guestAddress: this.formBuilder.group({
+          addressLine1: new FormControl('', {
+            validators: [Validators.required, Validators.minLength(5)],
+          }),
+          addressLine2: new FormControl(''),
+          // addressLine3: new FormControl(''),
+          city: new FormControl('', {
+            validators: [Validators.required],
+          }),
+          state: new FormControl('', {
+            validators: [Validators.required],
+          }),
+          country: new FormControl('', {
+            validators: [Validators.required],
+          }),
+          zipCode: new FormControl('', {
+            validators: [Validators.required],
+          }),
+        }),
+        // guestCount: new FormControl(''),
+        guestList: this.formBuilder.array([this.addGuestControl()], {
+          validators: [Validators.minLength(1), Validators.maxLength(4)],
+        }),
+      },
+      { updateOn: 'blur', validators: Validators.required }
+    );
+  }
+
   login() {
     if (this.loginService.login(this.email, this.password)) {
       // alert('Login successful!');
